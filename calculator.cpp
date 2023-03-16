6402948a4c0d1c465795c898b5bc0cea0ce20ef7
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-17 00:17:52 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,67 @@
+//type1
+//#include <stdio.h>
+//int main(){
+//    double a,b;
+//    char ch;
+//    printf("type what you do s=sum , d=diff , m=multi , D=divide\n");
+//    scanf("%c",&ch);
+//    printf("Enter 2 num \n");
+//    scanf("%lf%lf",&a,&b);
+//    
+//    
+//    
+//     switch(ch){
+//     case 's':
+//      printf("\nsum of %lf & %lf=%lf",a,b,a+b);
+//     break;
+//     case 'd':
+//      printf("\ndifferenece of %lf & %lf=%lf",a,b,a-b);
+//     break;
+//     case 'm':
+//      printf("\nmultiplication of %lf & %lf=%lf",a,b,a*b);
+//     break;
+//     case 'D':
+//      printf("\n%lf divide by %lf = %lf",a,b,a/b);
+//     break;
+//    default:
+//     printf("Invalid input");
+//     break;
+//}
+//return 0;
+//}
+
+//type=2
+//#include <stdio.h>
+//int main(){
+//    double a,b;
+//    char ch;
+//    printf("type what you do + for sum , - for diff , * for multi , / for divide\n");
+//    scanf("%c",&ch);
+//    printf("Enter 2 num \n");
+//    scanf("%lf%lf",&a,&b);
+//    
+//    
+//    
+//    
+//     switch(ch){
+//     case '+':
+//      printf("\nsum of %lf & %lf=%lf",a,b,a+b);
+//     break;
+//     case '-':
+//      printf("\ndifferenece of %lf & %lf=%lf",a,b,a-b);
+//     break;
+//     case '*':
+//      printf("\nmultiplication of %lf & %lf=%lf",a,b,a*b);
+//     break;
+//     case '/':
+//      printf("\n%lf divide by %lf = %lf",a,b,a/b);
+//     break;
+//    default:
+//     printf("Invalid input");
+//     break;
+//}
+//return 0;
+//}
+
+//type=3
+
