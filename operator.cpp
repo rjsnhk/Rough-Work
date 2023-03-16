cf513caa62ee7ab61d55121e3f9694751d5f3cff
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-17 00:19:44 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+//	int i=-3;
+//    printf("%d%d \n",i/-2,i%-2);
+//    int x=5%2;
+//    printf("%d \n",x);
+
+
+//    int a=10;
+//    double b=5.6;
+//    int c;
+//    c=a+b;
+//    printf("%d \n",a+b);
+
+//int x=10,b=5,c=5,d;
+//    d=b+c==x;
+//    printf("%d",d);
+
+
+
+//int x=10,b=0,c;
+//    c=x&&b;
+//    printf("%d",c);
+
+
+//int x=1,y=0,z=5,a;
+//a=x&&y||z++;
+//printf("%d",z);
+
+int x=1,y=0,z=5,a;
+a=x&&y&&z++;
+printf("%d",z);
+	return 0;
+}
