20d4e3a61c96547678f2f0041b57e5f17b549ec2
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-17 00:18:20 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,20 @@
+# include <stdio.h>
+void sum()
+int main(){
+	printf(sum());
+}
+void sum(){
+	int a=9,b=9;
+//	scanf("%d%d".&a,&b);
+	printf("sum=%d",a+b);
+}
+
+
+////type 2
+//#include <stdio.h>
+//int sum(int x,int y)
+//{
+//	int z=x+y;
+//	return z;
+//}
+//main
