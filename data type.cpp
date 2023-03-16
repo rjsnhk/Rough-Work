2f6e2f2eea7512786e439e344ea4a6603e0a9322
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-17 00:18:03 +0530

    Add files via upload

---
+++ data type.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+int main(){
+//    char ch;
+//    ch=321;
+//    printf("\n %c",66);
+//    printf("\n %d",' ');
+//    printf("\n %d",sizeof(ch));
+//    printf("\n size = %d B",sizeof(char)); //1
+//    printf("\n Value = %c ",ch); //A
+//    printf("\n Address = %d ",&ch); //6422303
+
+
+//	short int x;
+//    x=54;
+//    printf("\n %d",sizeof(x));
+//    printf("\n size int= %d B",sizeof(int));
+//    printf("\n size short int= %d B",sizeof(short int));
+//    printf("\n size long int= %d B",sizeof(long int));
+//    printf("\n size long long int= %d B",sizeof(long long int));
+//    printf("\n size long= %d B",sizeof(long));
+//    printf("\n Value = %d ",x);
+//    printf("\n Address = %d ",&x);
+//
+//    printf("\n %i",x);
+//    printf("\n %u",-100);
+//    printf("\n %d",-100);
+
+//    int a,b;
+//    printf("enter first num \n");
+//    scanf("%d",&a);
+//    printf("enter second num \n");
+//    scanf("%d",&b);
+//    int sum=a+b;
+//    printf("sum of 2 num is = %d \n",sum);
+//    printf("%d + %d = %d",a,b,sum);
+
+
+//find average of 3 num
+    // int a,b,c,average;
+    // printf("enter 1st num \n");
+    // scanf("%d",&a);
+    // printf("enter 2nd num \n");
+    // scanf("%d",&b);
+    // printf("enter 3rd num \n");
+    // scanf("%d",&c);
+    // average=(a+b+c)/3;
+    // printf("average %d \n",average);
+
+
+//2 input ek sath
+//    int a,b,sum;
+//    printf("enter two num \n");
+//    scanf("%d%d",&a,&b);
+//    sum=a+b;
+//    printf("sum of 2 num is = %d \n",sum);
+//    printf("%d + %d = %d",a,b,sum);
+    
+    
+    
+    
+    return 0;
+}
