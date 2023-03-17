3452611696ca2c8ea7010c14e9bb3547bf53daf1
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-18 00:03:24 +0530

    Add files via upload

---
+++ ques.cpp
@@ -0,0 +1,34 @@
+//#include<stdio.h>
+//int main(){
+//    short a;
+//    
+//    for(a=1;a>=0;a++)
+//       printf("%d\n",a);
+////     printf("%f",x);
+//    return 0;
+//}
+#include <stdio.h>
+    int main()
+    {	int i;
+        for (5;2;2)
+            printf("In for loop\n");
+    }
+//#include<stdio.h>
+//int main()
+//{
+//   int a=7,b=5;
+//   
+//   switch(a=a%b){
+//     case 1:
+//        a=a-b;
+//     
+//     case 2:
+//     	printf("%d",a);
+////        a=a+b;
+//     
+//    default:
+//     a=a;
+//     
+//    }
+//return 0;
+//}
