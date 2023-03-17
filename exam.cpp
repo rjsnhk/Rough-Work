c9afcf061add656a0f3e28aba95256514efc45b7
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-18 00:02:18 +0530

    Add files via upload

---
+++ exam.cpp
@@ -0,0 +1,502 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//  int a= 2;
+//  float x = 9.5;
+//  double y = 10.5;
+//  long double z = 50;
+//  double d;
+////   a = z/b+b*x-y;
+////   printf("When stored as integer : %d\n",a);
+////   d = z/b+b*x-y;
+////   printf("When stored as double : %f\n",d);
+//printf("%Lf",y);
+//    return 0;
+//}
+//#include  <stdio.h>
+//    int main()
+//    {
+//       signed char chr;
+//       chr = 129;
+//       printf("%d\n", chr);
+//       return 0;
+//    }
+//  #include <stdio.h>
+//     int main()
+//     {
+//         int i = -3;
+//         int l = i / -2;
+//         int k = i % -2;
+//         printf("%d %d\n", l, k);
+//         return 0;
+//     }
+//#include <stdio.h>
+//    int main()
+//    {
+//        int x = 5.3;
+//        printf("Value of x is %d", x);
+//        return 0;
+//    }
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        int a = 10;
+//        double b = 5.6;
+//        int c;
+//        c = a + b;
+//        printf("%d", c);
+//        return 0;
+//    }
+//#include <stdio.h>
+//    int main()
+//    {
+//        int a=1,b=1,c;
+//        c = a++ + b;
+//        printf("%d,%d,%d", a,b,c);
+//        return 0;
+//    }
+//#include <stdio.h>
+//    int main()
+//    {
+//        int a = 10, b = 5, c = 5;
+//        int d;
+//        d = b + c == a;
+//        printf("%d", d);
+//        return 0;
+//    }
+//#include <stdio.h>
+//    int main()
+//    {
+//        int a=2, b=3, x=0;
+//		x = (a++, 6);
+//
+//        printf("%d",x);
+//        printf("%d",a);
+//    }
+//#include <stdio.h>
+//  int main()
+//    {
+//        double b = 4*4/4*3/2;
+//        printf("%lf", b);
+//        return 0;
+//    }
+//#include<stdio.h>
+//int main()
+//{
+// int v=5;
+// for(int v=0;v<=10;v++){
+// 	
+// }
+//printf("%d",v);
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d",sizeof(unsigned int));
+//	printf("%d",sizeof(char));
+//	printf("%d",sizeof(float));
+//	printf("%d",sizeof(short int));
+//	printf("%d",sizeof(long int));
+//	printf("%d",sizeof(double));
+//	printf("%d",sizeof(long double));  
+//	printf("%d",sizeof(int));
+//	printf("%d",sizeof(int));
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//   int pt;
+//   printf("Enter the number of nodes:");
+//   scanf("%d", &pt);
+//   switch(pt){
+//     case 0:
+//      printf("\nNo Geometry");
+//     case 1:
+//      printf("\nA point");
+//     case 2:
+//      printf("\nA line");
+//     break;
+//     case 3:
+//      printf("\nA triangle");
+//     break;
+//     case 4:
+//      printf("\nA rectangle");
+//     break;
+//     case 5:
+//      printf("\nA pentagon");
+//     break;
+//    default:
+//     printf("Invalid input");
+//     break;
+//    }
+//return 0;
+//}
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        double ch;
+//        printf("enter a value between 1 to 2:");
+//        scanf("%lf", &ch);
+//        switch (ch)
+//        {
+//           case 1:
+//              printf("1");
+//              break;
+//           case 2:
+//              printf("2");
+//              break;
+//        }
+//        return 0;
+//    }
+
+// #include <stdio.h>
+//    int main()
+//    {
+//        char x = 'a';
+//        switch (x)
+//        {
+//           case 'a':
+//              printf("yes ");
+//              break;
+//           case 'a':
+//              printf("no");
+//              break;
+//        }
+//    }
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        int a = 1;
+//        switch (a)
+//        {
+//           case a:
+//              printf("Case A ");
+//           default:
+//              printf("Default");
+//        }
+//       return 0;
+//    }
+
+
+
+//#include <stdio.h>
+//
+//int main() {
+//    int x = 11;
+//    char y='a';
+//    printf("%d",3);
+//    printf("The value of x is: %d\n", x);
+//    printf("The size of the variable is: %lu\n", sizeof(x=x+1));
+//    printf("The value of x is: %d\n", x);
+//}
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        int i = 0;
+//        while (i < 3)
+////            i++;
+//        printf("In while loop\n");
+//        i++;
+//    }
+
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        int i = 0;
+//        while (i < 3){
+//        	i++;
+//        	printf("In while loop\n");
+//        
+//		}
+////            i++;
+//        printf("In while loop\n");
+//        i++;
+//    }
+
+// #include <stdio.h>
+//    int main()
+//    {
+//        int i = 12;
+//        while (i=10)
+//        {
+////            printf("H");
+//				i++;
+//        }
+//        printf("%d",i);
+//        return 0;
+//    }
+
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        int i = 0;
+//        while (++i)
+//        {
+////            printf("H");
+//        }
+//        printf("H");
+//        return 0;
+//    }
+
+//#include <stdio.h>   
+//int main() 
+//{ 
+//  int i;   
+//  for (i = 1; i != 10; i += 2) 
+//    printf("Hello"); 
+//  return 0; 
+//}
+
+
+//#include <stdio.h>   
+//int main() 
+//{ 
+//  int i;   
+//  while (printf("rajesh")){
+//  	
+//  } 
+//  return 0; 
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//int i;
+//for(i=1;i<10;i++);
+//printf("%d",i);
+//return 0;
+//}
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        do
+//            printf("In while loop ");
+//        while (0);
+//            printf("After loop\n");
+//        return 0;
+//    }
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        int i = 0;
+//        do {
+//            i++;
+//            printf("In while loop\n");
+//        } while (i < 3);
+//       return 0;
+//    }
+
+//#include <stdio.h>
+//
+//int main() 
+//{ 
+//printf ("%d %d %d %d",72,072,0x72,0X72); 
+//}
+
+
+//#include <stdio.h>
+//int main() 
+//{	
+//	int a = 30, b = 40, x; 
+//	x = (a!=10)&&(b = 50); 
+//	printf("x=%d",x);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//    int main()
+//    {
+//        int i = 0;
+//        do
+//        {
+//            i++;
+//            if (i == 2)
+//                continue;
+//                printf("In while loop ");
+//        } while (i < 2);
+//        printf("%d\n", i);
+//        return 0;
+//    }
+
+
+//#include<stdio.h>
+//int main()
+//{
+//int i=1;
+//while(i<=5)
+//{
+//    i++;
+//    continue;
+//    printf("\nC");
+//}
+//return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//int i;
+//for(i=1;i<=5;i++)
+//{
+//  printf("%d ",i);
+//  continue;
+//}
+//return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//int i=0;
+//for(;;)
+//{
+//    i++;
+//    if(i%3==0)
+//        continue;
+//    else if(i%5==0)
+//        break;
+//    printf("%d ",i);
+//
+//}
+//return 0;
+//}
+
+
+//#include <stdio.h>
+//int main(){
+//	 int n=10;
+//
+//A:
+//   printf("%d", n);
+//   n = n -1;
+// 
+//   if (n>10)
+//	 goto A;
+//
+//}
+
+
+//#include <stdio.h>
+//int main(){
+//	 int f=12.345;
+//	 int f=10;
+////	 scanf("%5f",&f);
+//	 printf("%d",f);
+//
+//}
+
+//#include <stdio.h>
+//int main() 
+//{ 
+//int a=6543; 
+//printf("*%5d,*%-5d*",a,a); 
+//return 0;
+// } 
+
+
+//int main()
+// {
+// int a=123; 
+//printf("*%06d*",a); 
+//return 0; 
+//}
+
+
+//int main()
+// {
+// float a=654.123456f;
+// printf("%3.3f,%3.2f",a,a);
+// return 0;
+// }
+
+//int main() 
+//{
+// char ch='A';
+// ch=getchar(); 
+//putchar(ch); 
+//return 0;
+// }
+
+//#include <stdio.h>
+//int main()
+//{
+//    int i=7;
+// printf("%d%d%d%d",i++,i,++i,i+1);}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//    void show()
+//    {
+//        printf("hello");
+//    }
+//    show();
+//}
+
+
+//#include <stdio.h>
+//void show();
+//int main()
+//{
+//    show();
+//    printf("hello");
+//}
+//void show()
+//{
+//    printf(" World");
+//}
+
+
+#include <stdio.h>
+void show();
+int main()
+{
+    show();
+    printf("hello");
+}
+void show()
+{
+   printf("World");
+    main();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
