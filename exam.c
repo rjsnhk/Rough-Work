9336c13af95e7ad37c3750951571efd55dd763d0
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-18 00:02:42 +0530

    Add files via upload

---
+++ exam.c
@@ -0,0 +1,114 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main()
+// {
+//   int a= 2;
+//   float x = 9.5;
+//   double y = 10.5;
+//   long double z = 50;
+//   double d;
+// //   a = z/b+b*x-y;
+// //   printf("When stored as integer : %d\n",a);
+// //   d = z/b+b*x-y;
+// //   printf("When stored as double : %f\n",d);
+// printf("%d",sizeof(z));
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//   int v=5;
+//   for(int v=0;v<=10;v++){
+ 	
+//   }
+// printf("%d",v);
+//  }
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("%d",sizeof('b'));
+//}
+
+
+//  #include <stdio.h>
+//     int main()
+//     {
+//         char x = 'a';
+//         switch (x)
+//         {
+//            case 'a':
+//               printf("yes ");
+              
+//            case 'b':
+//               printf("no");
+//               break;
+//         }
+//     }
+
+// #include <stdio.h>
+
+// int main() 
+// { 
+// printf ("%d %d %d %d",72,072,0x72,0X72);
+// }
+
+
+// #include <stdio.h>
+// int main() 
+// {	
+// 	int a = 30, b = 40, x; 
+// 	x = (a!=10)&&(b = 50); 
+// 	printf("x=%d",x);
+// 	return 0;
+// }
+
+
+// #include <stdio.h>
+// int main(){
+// 	 int n=10;
+
+// A:
+//    printf("%d", n);
+//    n = n -1;
+ 
+//    if (n>0)
+// 	 goto A;
+
+// }
+
+
+// #include<stdio.h>
+//  void main()
+// 	{
+// 	char ch[30];
+// 	printf("Enter the string:");
+// 	gets(ch);
+// 	printf("Entered string: %s", ch);
+// 	}
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int i=7;
+//  printf("%d%d%d%d",i++,i,++i,i+1);}
+
+// q24 mid term setA
+
+// #include<stdio.h>
+// void foo(float *);
+// int main(){
+//     int i=10,*p=&i;
+//     foo(&i);
+// }
+// void foo(float *p){
+//     printf("%f\n",*p);
+// }
+
+#include <stdio.h>
+int main(){
+    int x=1,y=0,z=3;
+    x>y?printf("%d",z):return z;
+}
