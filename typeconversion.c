dc52d7aae4c9f6b28333ff9b19f21a8c1daec71c
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-20 03:16:06 +0530

    Add files via upload

---
+++ typeconversion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int a,b = 2;
+  float x = 9.5;
+  double y = 10.5;
+  long int z = 50;
+  double d;
+  printf("%f",b*x);
+//   a = z/b+b*x-y;
+//   printf("When stored as integer : %d\n",a);
+//   d = z/b+b*x-y;
+//   printf("When stored as double : %f\n",d);
+    return 0;
+}
