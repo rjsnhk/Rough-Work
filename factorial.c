10949dbf6a8dfc86ea47c6eb8451dd2d0cd9e4ed
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-21 00:36:07 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,29 @@
+// #include<stdio.h>
+// int main(){
+//     int i,n,fact=1;
+//     printf("enter a num");
+//     scanf("%d",&n);
+//     for (i=1;i<=n;i++){
+//         fact=fact*i;
+//     }
+//     printf("%d",fact);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int i=1;
+// int main(){
+//     printf(" hello %d",i);
+//     i++;
+
+//     if(i<=10)
+//     main();
+//     --i;
+//     printf("\nbyr %d",i);
+//     return 0;
+// }
+#include <stdio.h>
+int main(){
+    
+    return 0;
+}
