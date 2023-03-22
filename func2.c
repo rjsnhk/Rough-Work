0922b5cda8d9cefc5183255f3120125373552022
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-22 19:15:46 +0530

    Add files via upload

---
+++ func2.c
@@ -0,0 +1,85 @@
+// // function without return-type and without parameter
+// #include <stdio.h>
+// void sum(); //declaration
+
+// void main()
+// {
+// sum();
+// }
+
+// void sum () // definition
+// {
+// int a,b,c;
+// printf("\n Enter two numbers:");
+// scanf("%d%d", &a,&b);
+// c=a+b;
+// printf("\n Sum = %d",c);
+// }
+
+
+
+
+// // function with return-type and without parameter
+// #include <stdio.h>
+// int sum(); //declaration
+
+// int main()
+// {
+// int c;
+// c= sum();
+// printf("\n Sum = %d",c);
+// }
+
+// int sum () // definition
+// {
+// int a,b;
+// printf("\n Enter two numbers:");
+// scanf("%d%d", &a,&b);
+// return (a+b);
+// }
+
+// ----
+
+// function without return-type and with parameter
+// #include <stdio.h>
+// void sum(int x, int y); //declaration
+
+// void main()
+// {
+// int a,b;
+// printf("\n Enter two numbers:");
+// scanf("%d%d", &a,&b);
+// sum(a,b);
+
+// }
+
+// void sum (int x, int y) // definition
+// {
+
+// printf("\n Sum = %d",x+y);
+// }
+
+
+// -----
+
+
+// // function with return-type and with parameter
+// #include <stdio.h>
+// int sum(int x, int y); //declaration
+
+// int main()
+// {
+// int a,b,c;
+// printf("\n Enter two numbers:");
+// scanf("%d%d", &a,&b);
+// c= sum(a,b);
+// printf("\n Sum = %d",c);
+// }
+
+// int sum (int x, int y) // definition
+// {
+// return (x+y);
+// }
+
+
+
