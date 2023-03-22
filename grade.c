fafaeddba2c7a50eca4df6b7981ec9aad34756ac
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-03-22 19:16:42 +0530

    Add files via upload

---
+++ grade.c
@@ -0,0 +1,51 @@
+// #include<stdio.h>
+// int main(){
+//     double mark;
+//     printf("enter your mark \n");
+//     scanf("%lf",&mark);
+//     if (mark>=90){
+//         printf("you got A");
+//     }
+//     else if (mark>=80 && mark<=89.99){
+//         printf("you got B");
+//     }
+//     else if (mark>=70 && mark<=79.99){
+//         printf("you got C");
+//     }
+//     else if (mark>=60 && mark<=69.99){
+//         printf("you got D");
+//     }
+//     else if (mark>=50 && mark<=59.99){
+//         printf("you got E");
+//     }
+//     else {
+//         printf("you are fail");
+//     }
+    
+//     return 0;
+// }
+
+
+// #include<stdio.h>
+// int main(){
+//     int age;
+//     printf("enter your age \n");
+//     scanf("%d",&age);
+//     return 0;
+// }
+
+
+#include <stdio.h>
+int main (){
+	int age;
+	printf("enter your age");
+	scanf("%d",&age);
+	if (age>18) {
+		printf("you are above 18");
+	}
+	
+    else{
+	printf("your are below 18");
+	
+}
+}
